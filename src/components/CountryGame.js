23a50b25ff048d86446d686f866c114f831712df
R-Currie <currierobertm@gmail.com>
2019-11-23 16:50:08 -0600

    Flag choices component done

--- src/components/CountryGame.js
+++ src/components/CountryGame.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FlagQuestion, { QuestionStates } from './FlagQuestion';
 
 class CountryGame extends React.Component {
     constructor(props){
@@ -11,7 +12,7 @@ class CountryGame extends React.Component {
         fetch("https://restcountries.eu/rest/v2/all")
             .then(response => response.json())
             .then(countries => {
-                const correctOption = Math.floor(Math.random * countries.length);
+                const correctOption = Math.floor(Math.random() * countries.length);
                 const options = this.getOptions(correctOption, countries);
                 this.setState({
                     countries,
@@ -21,20 +22,66 @@ class CountryGame extends React.Component {
             })
     }
 
+    onGuess(answer) {
+        const {correctOption} = this.state;
+        let questionState = answer === correctOption ?
+            QuestionStates.ANSWER_CORRECT : QuestionStates.ANSWER_WRONG;
+
+        this.setState({questionState});
+    }
+
+    nextQuestion() {
+        const {countries} = this.state;
+        const correctOption = Math.floor(Math.random() * countries.length);
+        const options = this.getOptions(correctOption, countries);
+        this.setState({
+            countries,
+            correctOption,
+            options,
+            questionState: QuestionStates.QUESTION
+        });
+    }
+
     getOptions(correctOption, countries){
         let options = [correctOption];
         while(options.length < 4){
-            let opt = Math.floor(Math.random * countries.length);
+            let opt = Math.floor(Math.random() * countries.length);
             if(options.indexOf(opt) === -1){
                 options.push(opt);
             }
         }
-        return options;
+        return options.sort(() => Math.random() - 0.5);
     }
 
     render(){
+        let {
+            countries, correctOption, options, questionState
+        } = this.state;
+
+        let output = <div>Loading...</div>;
+
+        if(correctOption !== undefined){
+            const {flag, name } = countries[correctOption];
+            console.log(countries[2].name)
+            let opts = options.map(opt => {
+                return {
+                    id: opt,
+                    name: countries[opt].name
+                };
+            });
+            output = (
+                <FlagQuestion
+                    answerText={name}
+                    onGuess={this.onGuess}
+                    onNext={this.nextQuestion}
+                    options={opts}
+                    questionState={questionState}
+                    flag={flag} />
+            );
+        }
+
         return(
-            <div>Country Game</div>
+            <div style={{marginTop: '15px'}}>{output}</div>
         )
     }
 }
---
+++ src/components/FlagChoices.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const FlagChoices = props => {
+    let options = props.options || [];
+    const {handleChange, handleSubmit} = props;
+    let inputs = options.map(input => (
+        <label key = {input.id}>
+            <input type="radio" value={input.id} checked={input.checked} onChange={handleChange} name="flag-choice" />
+            {input.name}
+        </label>
+    ));
+
+    return (
+        <form onSubmit={handleSubmit}>
+            {inputs}
+            <button text="guess" type="submit" />
+        </form>
+    )
+}
+
+export default FlagChoices;
